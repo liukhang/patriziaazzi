6187c634878e6bfad6cb3e778e4922d0680dfa17
liukhang <liukhang123@gmail.com>
2018-05-27 23:29:54 +0700

    update all

---
+++ skin/frontend/rwd/patriziaazzi_live/jqtransformplugin/patrizia.js
@@ -0,0 +1,8 @@
+jQuery(function(){
+    jQuery('.policy').jqTransform({imgPath:'img/'});
+    jQuery('.input-box.custom-box').jqTransform({imgPath:'img/'});
+    jQuery('.qty-wrapper.custom-box').jqTransform({imgPath:'img/'});
+    jQuery('ol#checkoutSteps li.control').jqTransform({imgPath:'img/'});
+    jQuery('#co-payment-form dl dt').jqTransform({imgPath:'img/'});
+    jQuery('.fattura').jqTransform({imgPath:'img/'});
+});
